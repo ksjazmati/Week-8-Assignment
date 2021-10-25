a1b843a41ecbb7972faf8603397725872214735d
ksjazmati <34400446+ksjazmati@users.noreply.github.com>
2021-10-25 19:22:43 -0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,11 +1,12 @@
+#1. Stores your first name as a variable. Use all lowercase letters when you declare it.
 first = "karam"
+#2. Stores your last name as a variable. Use all uppercase letters when you declare it.
 last = "JAZMATI"
+#3. Prints out, "Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions.
 print("Hello", first.upper(),last.lower())
+#4. Prints out two newlines.
 print("\n\n")
-
+#5. Creates a new variable that stores your first and last name together with a space between both parts.
 name = (first + " " + last)
-
-
-del name[last]
-
+#6. Slices your last name from the variable you created in step 5 and prints it out. This must take place on one line
 print(name)
